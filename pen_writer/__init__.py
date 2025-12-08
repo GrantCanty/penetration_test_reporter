968d8f7e05dcd20d8c4cac56a947c53639492243
Grant Canty <gc933815@ohio.edu>
2025-12-08 16:14:54 +0100

    added extra error codes to __init__.py file for the summarization feature

--- pen_writer/__init__.py
+++ pen_writer/__init__.py
@@ -4,10 +4,14 @@ __version__ = "0.1.0"
 (
     SUCCESS,
     TARGET_ERROR,
-    RESPONSE_ERROR
-) = range(3)
+    RESPONSE_ERROR,
+    DIRECTORY_ERROR,
+    LLM_ERROR,
+) = range(5)
 
 ERRORS = {
     TARGET_ERROR: "URL provided is not valid",
-    RESPONSE_ERROR: "Error getting response from provided target"
+    RESPONSE_ERROR: "Error getting response from provided target",
+    DIRECTORY_ERROR: "Error with the provided directory",
+    LLM_ERROR: "Error when generating a response from the LLM"
 }
